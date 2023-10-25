b2cafb8eb5d89debbbe114183c5279a2d945a4ba
Vadim Safronov <109957474+gb2975vadim@users.noreply.github.com>
2023-10-25 21:26:55 +0300

    hw6

---
+++ homework_6/task.py
@@ -0,0 +1,18 @@
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+    while low <= high:
+        mid = (high + low) // 2
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+    return -1
+
+
+arr = [1, 2, 3, 4, 5]
+x = 5
+print(binary_search(arr, x))
